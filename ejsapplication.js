bde42ffeb4947a99883cf03c7ef377f44b2cda4b
SurajKedar-2003 <surajkedar658@gmail.com>
2024-07-07 18:10:18 +0530

    create course info and add header&footer dynamically

---
+++ ejsapplication.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('pages/home');
+})
+
+const courses = [
+    {
+        id:101,
+        cname: 'web',
+        duration:"3 months",
+        fees: 20000
+    },
+    {
+        id:102,
+        cname: 'mern stack',
+        duration:"4 months",
+        fees: 30000
+    },
+    {
+        id:103,
+        cname: 'java',
+        duration:"4 months",
+        fees: 45000
+    },
+    {
+        id:104,
+        cname: 'testing',
+        duration:"5 months",
+        fees: 35000
+    }
+]
+
+app.get('/courses', (req, res)=>{
+    res.render('pages/course', {data:courses})
+})
+
+app.listen(3000);
+console.log('server is listening...');
